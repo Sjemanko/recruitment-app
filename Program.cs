2e437ca878562f27d8d5855b26f77bceaeaebd76
Sjemanko <tomekbu.ecki@gmail.com>
2023-11-19 17:55:59 +0100

    Connected to postgresql, added config

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using recruitment_app.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
