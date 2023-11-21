611ce33979855f702be0e8fb5bbf5c73a45a2101
Sjemanko <tomekbu.ecki@gmail.com>
2023-11-21 15:58:41 +0100

    changed config with docs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using recruitment_app.Models;
 
 namespace recruitment_app.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-        public DbSet<NewClasstest> Test { get; set; }
+        // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        // {
+        // }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=recruitment-app;User ID=postgres;Password=password;");
+
     }
 }
